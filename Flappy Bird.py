81327ce4001a8cb43cb1c3329133e33da3390326
Manan Shah <manan.vinod.shah@gmail.com>
2020-07-13 22:26:24 +0530

    Update Flappy Bird.py

--- Flappy Bird.py
+++ Flappy Bird.py
@@ -1,1 +1,38 @@
 #Still Working on it
+import os
+import pygame
+import neat
+import time
+import random
+
+WIN_HEIGHT = 600
+WIN_WIDTH = 800
+
+BIRD_IMGS = [pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(os.path.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(os.path.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bg.png")))
+
+
+class Bird:
+	IMGS = BIRD_IMGS
+	MAX_ROTATION = 25
+	ROT_VEL = 20
+	ANIMATION_TIME = 5
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.tilt = 0
+		self.tick_count = 0
+		self.vel = 0 
+		self.height = self.y
+		self.img = self.IMGS[0]
+	
+	def jump(self):
+		self.vel = -11
+		self.tick_count = 0
+		self.height = self.y
+
+	def move(self):
+		self.tick_count += 1 
