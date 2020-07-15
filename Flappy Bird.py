aed65bd5e66ac5b64416db39422810b1bc8a15b2
Manan Shah <manan.vinod.shah@gmail.com>
2020-07-15 20:10:47 +0530

    Update Flappy Bird.py

--- Flappy Bird.py
+++ Flappy Bird.py
@@ -5,13 +5,13 @@ import neat
 import time
 import random
 
-WIN_HEIGHT = 600
-WIN_WIDTH = 800
+WIN_HEIGHT = 800
+WIN_WIDTH = 600
 
-BIRD_IMGS = [pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bird3.png")))]
-PIPE_IMG = pygame.transform.scale2x(os.path.load(os.path.join("imgs", "pipe.png")))
-BASE_IMG = pygame.transform.scale2x(os.path.load(os.path.join("imgs", "base.png")))
-BG_IMG = pygame.transform.scale2x(os.path.load(os.path.join("imgs", "bg.png")))
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
 
 class Bird:
@@ -27,10 +27,11 @@ class Bird:
 		self.tick_count = 0
 		self.vel = 0 
 		self.height = self.y
+		self.img_count = 0
 		self.img = self.IMGS[0]
 	
 	def jump(self):
-		self.vel = -11
+		self.vel = -10.5
 		self.tick_count = 0
 		self.height = self.y
 
@@ -46,3 +47,58 @@ class Bird:
 			d -= 2
 
 		self.y = self.y + d
+
+		if d < 0 or self.y1 < self.height + 50:
+			if self.tilt < self.MAX_ROTATION:
+				self.tilt = sellf.MAX_ROTATION
+		else:
+			if self.tilt > -90:
+				self.tilt -= self.ROT_VEL
+
+	def draw(self, win):
+		self.img_count += 1
+
+		if self.img_count < self.ANIMATION_TIME:
+			self.img = self.IMGS[0]
+		elif self.img_count < self.ANIMATION_TIME*2:
+			self.img = self.IMGS[1]
+		elif self.img_count < self.ANIMATION_TIME*3:
+			self.img = self.IMGS[2]
+		elif self.img_count < self.ANIMATION_TIME*4:
+			self.img = self.IMGS[1]
+		elif self.img_count == self.ANIMATION_TIME*4+1:
+			self.img = self.IMGS[0]
+			self.img_count = 0
+
+		if self.tilt <= -80:
+			self.img = self.IMGS[1]
+			self.img_count = self.ANIMATION_TIME*2
+
+		rotated_image = pygame.transform.rotate(self.img, self.tilt)
+		new_rect = rotated_image.get_rect(center = self.img.get_rect(topleft = (self.x, self.y)).center)	
+		
+		win.blit(rotated_image, new_rect.topleft)
+
+		def get_mask(self):
+			return pygame.mask.from_surface(self.img)
+
+def draw_window(win, bird):
+	win.blit(BG_IMG, (0, 0))
+	bird.draw(win)
+	pygame.display.update()
+
+def main():
+
+	bird = Bird(200, 200)
+	win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+
+	run = True
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run =False
+		draw_window(win, bird)
+
+	pygame.quit()
+	quit()
+main()
